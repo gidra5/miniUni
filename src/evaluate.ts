a0a31dbdc8bc2dc24668b3a94cac8db5238d045e
gidra5 <hranitel.52@gmail.com>
2024-07-18 18:37:29 +0300

    feat: evaluate

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -1,13 +1,389 @@
-import { parseScript } from './parser';
+import { OperatorType, parseScript, type AbstractSyntaxTree } from './parser';
 import { parseTokens } from './tokens';
+import { assert } from './utils';
+
+// type Continuation = (arg: EvalValue) => Promise<EvalValue>;
+type EvalFunction = (arg: EvalValue) => Promise<EvalValue>;
+type EvalValue =
+  | number
+  | string
+  | boolean
+  | null
+  | EvalValue[]
+  | EvalFunction
+  | { channel: symbol };
+type Channel = {
+  queue: EvalValue[];
+  onReceive: Array<(v: EvalValue) => void>;
+};
+type Context = {
+  env: Record<string, EvalValue>;
+  channels: Record<symbol, Channel>;
+};
+
+// export const fn =
+//   (_fn: (arg: EvalValue) => Promise<EvalValue>): EvalFunction =>
+//   async (arg, cont) =>
+//     cont(await _fn(arg));
+
+export const newContext = (): Context => {
+  const channels = {};
+  return {
+    env: {
+      channel: async () => {
+        const channel = Symbol();
+        return { channel };
+      },
+    },
+    channels,
+  };
+};
+
+export const assign = async (
+  patternAst: AbstractSyntaxTree,
+  value: EvalValue,
+  context: Context
+): Promise<Context> => {
+  const env = { ...context.env };
+  if (patternAst.name === 'identifier') {
+    const name = patternAst.data.value;
+    if (!(name in env)) {
+      assert(
+        false,
+        `can't assign to undeclared variable: ${patternAst.data.value}`
+      );
+    }
+    env[name] = value;
+  }
+  context.env = env;
+  return context;
+};
+
+export const bind = async (
+  patternAst: AbstractSyntaxTree,
+  value: EvalValue,
+  context: Context
+): Promise<Context> => {
+  const env = { ...context.env };
+  if (patternAst.name === 'identifier') {
+    const name = patternAst.data.value;
+    env[name] = value;
+  }
+  context.env = env;
+  return context;
+};
+
+export const evaluateExpr = async (
+  ast: AbstractSyntaxTree,
+  context: Context = newContext()
+): Promise<EvalValue> => {
+  switch (ast.name) {
+    case 'operator': {
+      switch (ast.data.operator) {
+        case OperatorType.PATTERN:
+          assert(
+            false,
+            "destructuring patterns can't be evaluated by themselves"
+          );
+
+        case OperatorType.ADD: {
+          const args = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return args.reduce((acc, arg) => acc + arg);
+        }
+        case OperatorType.SUB: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left - right;
+        }
+        case OperatorType.MULT: {
+          const args = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return args.reduce((acc, arg) => acc * arg);
+        }
+        case OperatorType.DIV: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left / right;
+        }
+        case OperatorType.MOD: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left % right;
+        }
+        case OperatorType.POW: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left ** right;
+        }
+        case OperatorType.MINUS: {
+          const arg = evaluateExpr(ast.children[0], context);
+          return -arg;
+        }
+        case OperatorType.PLUS: {
+          const arg = evaluateExpr(ast.children[0], context);
+          return +arg;
+        }
+
+        case OperatorType.AND: {
+          const [head, ...rest] = ast.children;
+          const result = await evaluateExpr(head, context);
+          if (!result) return false;
+          while (rest.length > 0) {
+            const next = rest.shift();
+            assert(next, 'missing expression in and operator');
+            const result = await evaluateExpr(next, context);
+            if (!result) return false;
+          }
+          return true;
+        }
+        case OperatorType.OR: {
+          const [head, ...rest] = ast.children;
+          const result = await evaluateExpr(head, context);
+          if (result) return true;
+          while (rest.length > 0) {
+            const next = rest.shift();
+            assert(next, 'missing expression in or operator');
+            const result = await evaluateExpr(next, context);
+            if (result) return true;
+          }
+          return false;
+        }
+        case OperatorType.EQUAL: {
+          const [left, right] = await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          );
+          return left === right;
+        }
+        case OperatorType.NOT_EQUAL: {
+          const [left, right] = await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          );
+          return left !== right;
+        }
+        case OperatorType.LESS: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left < right;
+        }
+        case OperatorType.LESS_EQUAL: {
+          const [left, right] = (await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          )) as number[];
+          return left <= right;
+        }
+        case OperatorType.NOT: {
+          const arg = await evaluateExpr(ast.children[0], context);
+          return !arg;
+        }
+        case OperatorType.PARENS:
+          return await evaluateExpr(ast.children[0], context);
+
+        case OperatorType.INDEX: {
+          const [list, index] = await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          );
+          assert(Array.isArray(list), 'indexing on non-list');
+          assert(
+            typeof index === 'number' && Number.isInteger(index),
+            'index is not an integer'
+          );
+          return list[index];
+        }
+        case OperatorType.TUPLE: {
+          const list = await Promise.all(
+            ast.children.map(async (child) => {
+              if (child.data.operator === OperatorType.SPREAD)
+                return await evaluateExpr(child.children[0], context);
+              return [await evaluateExpr(child, context)];
+            })
+          );
+          return list.flat();
+        }
+        case OperatorType.SPREAD:
+          assert(
+            false,
+            'spread operator can only be used during tuple construction'
+          );
+
+        case OperatorType.PRINT: {
+          const value = await evaluateExpr(ast.children[0], context);
+          console.log(value);
+          return value;
+        }
+
+        case OperatorType.PARALLEL: {
+          ast.children.forEach((child) => evaluateExpr(child, { ...context }));
+          return null;
+        }
+
+        case OperatorType.SEND: {
+          const [channelValue, value] = await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          );
+          assert(
+            channelValue &&
+              typeof channelValue === 'object' &&
+              'channel' in channelValue,
+            'send operator on non-channel'
+          );
+          const symbol = channelValue.channel;
+          if (!(symbol in context.channels)) {
+            context.channels[symbol] = {
+              queue: [],
+              onReceive: [],
+            };
+          }
+          const channel = context.channels[symbol];
+
+          const onReceive = channel.onReceive.shift();
+          if (onReceive) onReceive(value);
+          else channel.queue.push(value);
+        }
+        case OperatorType.RECEIVE: {
+          const channelValue = await evaluateExpr(ast.children[0], context);
+          assert(
+            channelValue &&
+              typeof channelValue === 'object' &&
+              'channel' in channelValue,
+            'receive operator on non-channel'
+          );
+          const symbol = channelValue.channel;
+          if (!(symbol in context.channels)) {
+            context.channels[symbol] = {
+              queue: [],
+              onReceive: [],
+            };
+          }
+          const channel = context.channels[symbol];
+
+          if (channel.queue.length > 0) {
+            return channel.queue.shift()!;
+          }
+
+          return await new Promise((resolve) => {
+            channel.onReceive.push(resolve);
+          });
+        }
+
+        case OperatorType.TOKEN:
+          assert(false, 'token operator should only be used during parsing');
+
+        case OperatorType.IF: {
+          const [condition, branch] = ast.children;
+          const result = await evaluateExpr(condition, context);
+          if (result) {
+            return await evaluateExpr(branch, context);
+          }
+          return null;
+        }
+        case OperatorType.IF_ELSE: {
+          const [condition, trueBranch, falseBranch] = ast.children;
+          const result = await evaluateExpr(condition, context);
+          if (result) return await evaluateExpr(trueBranch, context);
+          else return await evaluateExpr(falseBranch, context);
+        }
+        case OperatorType.WHILE: {
+          const [condition, body] = ast.children;
+          let result: EvalValue = null;
+          while (await evaluateExpr(condition, context)) {
+            result = await evaluateExpr(body, context);
+          }
+          return result;
+        }
+        case OperatorType.ASSIGN: {
+          const [pattern, expr] = ast.children;
+          const value = await evaluateExpr(expr, context);
+          assign(pattern, value, context);
+          return value;
+        }
+        case OperatorType.DECLARE: {
+          const [pattern, expr] = ast.children;
+          const value = await evaluateExpr(expr, context);
+          bind(pattern, value, context);
+          return value;
+        }
+        case OperatorType.BLOCK:
+          return await evaluateSequence(ast, { ...context });
+
+        case OperatorType.FUNCTION: {
+          const [{ children: patterns }, body] = ast.children;
+          const binder = (
+            patterns: AbstractSyntaxTree[],
+            context: Context
+          ): EvalFunction => {
+            return async (arg) => {
+              const [pattern, ...rest] = patterns;
+              const bound = await bind(pattern, arg, { ...context });
+              if (rest.length === 0) {
+                return await evaluateExpr(body, bound);
+              }
+              return binder(rest, bound);
+            };
+          };
+          return binder(patterns, context);
+        }
+        case OperatorType.APPLICATION: {
+          const [fnValue, argValue] = await Promise.all(
+            ast.children.map((child) => evaluateExpr(child, context))
+          );
+
+          assert(
+            typeof fnValue === 'function',
+            'application on not a function'
+          );
+
+          return await fnValue(argValue);
+        }
+      }
+    }
+
+    case 'name':
+      return context.env[ast.data.value];
+    case 'number':
+    case 'string':
+      return ast.data.value;
+    case 'placeholder':
+    case 'implicitPlaceholder':
+      assert(false, "placeholder can't be evaluated");
+    case 'error':
+      assert(false, `parsing error: ${ast.data.cause.display()}`);
+    default:
+      return null;
+  }
+};
+
+export const evaluateSequence = async (
+  ast: AbstractSyntaxTree,
+  context: Context = newContext()
+): Promise<EvalValue> => {
+  let result: EvalValue = null;
+  for (const child of ast.children) {
+    result = await evaluateExpr(child, context);
+  }
+  return result;
+};
+
+export const evaluateScript = async (
+  ast: AbstractSyntaxTree,
+  context: Context = newContext()
+): Promise<EvalValue> => {
+  return await evaluateSequence(ast, context);
+};
 
 export const evaluateScriptString = async (
   input: string,
-  context: unknown = {}
-) => {
+  context: Context = newContext()
+): Promise<EvalValue> => {
   const tokens = parseTokens(input);
   const ast = parseScript(tokens);
 
-  // return 0;
-  throw new Error('Not implemented');
+  return await evaluateScript(ast, context);
 };
