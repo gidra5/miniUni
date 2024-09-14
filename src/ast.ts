27166acad623731395e864e752591758496f6ec8
gidra5 <hranitel.52@gmail.com>
2024-09-14 19:47:19 +0300

    wip

--- src/ast.ts
+++ src/ast.ts
@@ -96,6 +96,201 @@ export const token = (token: Token, position: Position): AbstractSyntaxTree =>
     ? tokenError(token, position)
     : name(token.src, position);
 
+export enum OperatorType {
+  ADD = 'add',
+  PLUS = 'plus',
+  SUB = 'subtract',
+  MINUS = 'minus',
+  DIV = '/',
+  MULT = '*',
+  MOD = '%',
+  POW = '^',
+  PARALLEL = 'parallel',
+  RECEIVE = 'receive',
+  SEND = 'send',
+  DECLARE = ':=',
+  ASSIGN = '=',
+  ATOM = 'atom',
+  COLON = ':',
+  TUPLE = ',',
+  SPREAD = '...',
+  NOT = 'not',
+  NOT_EQUAL = '!=',
+  EQUAL = '==',
+  DEEP_EQUAL = '===',
+  DEEP_NOT_EQUAL = '!==',
+  AND = 'and',
+  OR = 'or',
+  LESS = '<',
+  LESS_EQUAL = '<=',
+  APPLICATION = 'application',
+  PARENS = 'parens',
+  OBJECT = 'object',
+  INDEX = 'index',
+  SQUARE_BRACKETS = 'square_brackets',
+  SEQUENCE = 'sequence',
+  BLOCK = 'block',
+  FUNCTION = 'func',
+  IF = 'if',
+  IF_ELSE = 'if_else',
+  WHILE = 'while',
+  TOKEN = 'token',
+  IMPORT = 'import',
+  POST_INCREMENT = 'post_increment',
+  POST_DECREMENT = 'post_decrement',
+  DECREMENT = '--',
+  INCREMENT = '++',
+  EXPORT = 'export',
+  RECEIVE_STATUS = '<-?',
+  SEND_STATUS = '?<-',
+  INC_ASSIGN = '+=',
+  LOOP = 'loop',
+  FOR = 'for',
+  ASYNC = 'async',
+  MATCH = 'match',
+  MATCH_CASE = 'match_case',
+  INJECT = 'inject',
+  MASK = 'mask',
+  WITHOUT = 'without',
+  GREATER = '>',
+  GREATER_EQUAL = '>=',
+  MUTABLE = 'mut',
+}
+
+enum Associativity {
+  LEFT = 'left',
+  RIGHT = 'right',
+  LEFT_AND_RIGHT = 'both',
+}
+
+enum Fixity {
+  PREFIX = 'prefix',
+  POSTFIX = 'postfix',
+  INFIX = 'infix',
+  NONE = 'none',
+}
+
+// if two same operators are next to each other, which one will take precedence
+// first come lower precedence operators
+// const semicolonPrecedence = [
+const precedenceList: [OperatorType, Fixity, Associativity?][] = [
+  [OperatorType.SEQUENCE, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.FUNCTION, Fixity.PREFIX],
+  [OperatorType.IF, Fixity.PREFIX],
+  [OperatorType.IF_ELSE, Fixity.PREFIX],
+  [OperatorType.LOOP, Fixity.PREFIX],
+  [OperatorType.WHILE, Fixity.PREFIX],
+  [OperatorType.FOR, Fixity.PREFIX],
+  [OperatorType.MATCH, Fixity.PREFIX],
+  [OperatorType.MATCH_CASE, Fixity.PREFIX],
+  [OperatorType.INJECT, Fixity.PREFIX],
+  [OperatorType.MASK, Fixity.PREFIX],
+  [OperatorType.WITHOUT, Fixity.PREFIX],
+  [OperatorType.PARALLEL, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.ASYNC, Fixity.PREFIX],
+  // ] as const;
+
+  // const assignmentPrecedence = [
+  [OperatorType.DECLARE, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.ASSIGN, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.INC_ASSIGN, Fixity.INFIX, Associativity.RIGHT],
+  // ] as const;
+
+  // const tuplePrecedence = [
+  [OperatorType.TUPLE, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.COLON, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.SPREAD, Fixity.PREFIX],
+
+  [OperatorType.SEND, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.RECEIVE, Fixity.PREFIX],
+  [OperatorType.SEND_STATUS, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.RECEIVE_STATUS, Fixity.PREFIX],
+  // ] as const;
+
+  // const booleanPrecedence = [
+  [OperatorType.OR, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.AND, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.EQUAL, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.NOT_EQUAL, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.LESS, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.LESS_EQUAL, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.GREATER, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.GREATER_EQUAL, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.NOT, Fixity.PREFIX],
+  // ] as const;
+
+  // const arithmeticPrecedence = [
+  [OperatorType.ADD, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.SUB, Fixity.INFIX, Associativity.LEFT],
+  [OperatorType.MULT, Fixity.INFIX, Associativity.LEFT_AND_RIGHT],
+  [OperatorType.DIV, Fixity.INFIX, Associativity.LEFT],
+  [OperatorType.MOD, Fixity.INFIX, Associativity.LEFT],
+  [OperatorType.POW, Fixity.INFIX, Associativity.RIGHT],
+  [OperatorType.MINUS, Fixity.PREFIX],
+  [OperatorType.PLUS, Fixity.PREFIX],
+  [OperatorType.INCREMENT, Fixity.PREFIX],
+  [OperatorType.DECREMENT, Fixity.PREFIX],
+  [OperatorType.POST_INCREMENT, Fixity.POSTFIX],
+  [OperatorType.POST_DECREMENT, Fixity.POSTFIX],
+  // ] as const;
+
+  // const topPrecedence = [
+  [OperatorType.IMPORT, Fixity.PREFIX],
+  [OperatorType.EXPORT, Fixity.PREFIX],
+  [OperatorType.MUTABLE, Fixity.PREFIX],
+  [OperatorType.INDEX, Fixity.POSTFIX],
+  [OperatorType.APPLICATION, Fixity.INFIX, Associativity.LEFT],
+  [OperatorType.ATOM, Fixity.PREFIX],
+] as const;
+
+// if two same operators are next to each other, which one will take precedence
+// left associative - left one will take precedence
+// right associative - right one will take precedence
+// associative - does not matter, can be grouped in any order
+const leftAssociative = (precedence: number): Precedence => [
+  precedence,
+  precedence + 1,
+];
+const rightAssociative = (precedence: number): Precedence => [
+  precedence + 1,
+  precedence,
+];
+const associative = (precedence: number): Precedence => [
+  precedence,
+  precedence,
+];
+
+const precedences = (() => {
+  const precedences = {};
+
+  const leftAssociative = (p: number): Precedence => [p, p + 1];
+  const rightAssociative = (p: number): Precedence => [p + 1, p];
+  const associative = (p: number): Precedence => [p, p];
+  let precedenceCounter = 0;
+  let prevFixity: Fixity = Fixity.NONE;
+  for (const [operator, fixity, associativity] of precedenceList) {
+    if (fixity !== prevFixity) {
+      precedenceCounter++;
+      prevFixity = fixity;
+    }
+
+    precedences[operator] =
+      fixity === Fixity.PREFIX
+        ? [null, precedenceCounter]
+        : fixity === Fixity.POSTFIX
+        ? [precedenceCounter, null]
+        : fixity === Fixity.NONE
+        ? [null, null]
+        : associativity === Associativity.LEFT_AND_RIGHT
+        ? associative(precedenceCounter)
+        : associativity === Associativity.LEFT
+        ? leftAssociative(precedenceCounter++)
+        : rightAssociative(precedenceCounter++);
+  }
+
+  return precedences as Record<OperatorType, Precedence>;
+})();
+
 export const operator = (
   operator: string | symbol,
   position: Position,
@@ -131,8 +326,10 @@ export const operator = (
       case OperatorType.ASSIGN:
         return [null, 1];
       case OperatorType.PARENS:
+        return [null, null];
       case OperatorType.SEQUENCE:
         return [null, null];
+      // return rightAssociative(semicolonPrecedence);
       case OperatorType.APPLICATION:
         return leftAssociative(maxPrecedence - 2);
       case OperatorType.INDEX:
@@ -203,6 +400,7 @@ export const operator = (
         return [null, arithmeticPrecedence + 7];
       case OperatorType.ATOM:
         return [null, maxPrecedence - 3];
+        return precedences[operator];
       default:
         return [null, null];
     }
@@ -250,78 +448,3 @@ export const tuple = (
     ...children.map((child) => child.data.position)
   )
 ): AbstractSyntaxTree => operator(OperatorType.TUPLE, position, ...children);
-
-export enum OperatorType {
-  ADD = 'add',
-  PLUS = 'plus',
-  SUB = 'subtract',
-  MINUS = 'minus',
-  DIV = '/',
-  MULT = '*',
-  MOD = '%',
-  POW = '^',
-  PARALLEL = 'parallel',
-  RECEIVE = 'receive',
-  SEND = 'send',
-  DECLARE = ':=',
-  ASSIGN = '=',
-  ATOM = 'atom',
-  COLON = ':',
-  TUPLE = ',',
-  SPREAD = '...',
-  NOT = 'not',
-  NOT_EQUAL = '!=',
-  EQUAL = '==',
-  AND = 'and',
-  OR = 'or',
-  LESS = '<',
-  LESS_EQUAL = '<=',
-  APPLICATION = 'application',
-  PARENS = 'parens',
-  OBJECT = 'object',
-  INDEX = 'index',
-  SEQUENCE = 'sequence',
-  BLOCK = 'block',
-  FUNCTION = 'func',
-  IF = 'if',
-  IF_ELSE = 'if_else',
-  WHILE = 'while',
-  TOKEN = 'token',
-  IMPORT = 'import',
-  POST_INCREMENT = 'post_increment',
-  POST_DECREMENT = 'post_decrement',
-  DECREMENT = '--',
-  INCREMENT = '++',
-  EXPORT = 'export',
-  RECEIVE_STATUS = '<-?',
-  SEND_STATUS = '?<-',
-  INC_ASSIGN = '+=',
-  LOOP = 'loop',
-  FOR = 'for',
-  ASYNC = 'async',
-  MATCH = 'match',
-  MATCH_CASE = 'match_case',
-  INJECT = 'inject',
-  MASK = 'mask',
-  WITHOUT = 'without',
-  GREATER = '>',
-  GREATER_EQUAL = '>=',
-  MUTABLE = 'mut',
-}
-
-// if two same operators are next to each other, which one will take precedence
-// left associative - left one will take precedence
-// right associative - right one will take precedence
-// associative - does not matter, can be grouped in any order
-export const leftAssociative = (precedence: number): Precedence => [
-  precedence,
-  precedence + 1,
-];
-export const rightAssociative = (precedence: number): Precedence => [
-  precedence + 1,
-  precedence,
-];
-export const associative = (precedence: number): Precedence => [
-  precedence,
-  precedence,
-];
